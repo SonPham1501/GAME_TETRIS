d502aea367a910a3ac3ba8c3be5527cd3a45e5b1
SON <son@SONs-Mac-mini.local>
2022-08-17 17:48:56 +0700

    setup project

---
+++ lib/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:game_tetris/screens/home/home_screen.dart';
+
+class App extends StatefulWidget {
+  const App({ Key? key }) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Game Tetris',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const Scaffold(
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
