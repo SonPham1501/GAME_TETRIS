2d332d587592e66bc9b0365cea85fa1dab2ccee0
SonPham1501 <bomditimvk02@gmail.com>
2022-08-26 23:04:55 +0700

    theme app bar

--- lib/widgets/theme_setting_bar.dart
+++ lib/widgets/theme_setting_bar.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_portal/flutter_portal.dart';
 import 'package:game_tetris/features/playboard/playboard.dart';
+import 'package:game_tetris/material/app_setting/app_setting_controller.dart';
+import 'package:game_tetris/material/audios/general_audio_controller.dart';
 import 'package:game_tetris/widgets/buttons/buttons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -16,7 +18,12 @@ class ThemeSettingBar extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final playboardInfoController = ref.watch(playboardInfoControllerProvider.notifier);
     final playboardDefaultColor = ref.watch(playboardInfoControllerProvider.select((value) => value.buttonColors));
+    final appSettingController = ref.watch(appSettingControllerProvider.notifier);
+    final isDark = ref.watch(appSettingControllerProvider.select((value) => value.isDarkTheme));
+    final audioController = ref.watch(generalAudioControllerProvider.notifier);
+    final isMute = ref.watch(generalAudioControllerProvider.select((value) => value.isMuted));
     final isColorPickerExpand = useState(false);
     final isPreCloseColorPicker = useState(false);
     return Padding(
@@ -57,13 +64,22 @@ class ThemeSettingBar extends HookConsumerWidget {
                             child: child!,
                           );
                         },
-                        child: IgnorePointer(
-                          ignoring: isPreCloseColorPicker.value,
-                          child: GameButton(
-                            key: ValueKey('color-${color.index}'),
-                            color: color,
-                            onPressed: () {  },
-                            child: Text(color.name[0].toUpperCase()),
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            bottom: color.index == ButtonColors.values.length -1
+                              ? 0
+                              : 8,
+                            left: (190 - 49 * 3) / 2,
+                          ),
+                          child: IgnorePointer(
+                            ignoring: isPreCloseColorPicker.value,
+                            child: GameButton(
+                              key: ValueKey('color-${color.index}'),
+                              color: color,
+                              size: ButtonSize.square,
+                              onPressed: () => playboardInfoController.buttonColors = color,
+                              child: Text(color.name[0].toUpperCase()),
+                            ),
                           ),
                         ),
                       );
@@ -92,6 +108,30 @@ class ThemeSettingBar extends HookConsumerWidget {
                     ? const Icon(Icons.arrow_circle_up_outlined)
                     : const Icon(Icons.arrow_circle_down_outlined),
                 ),
+              ),
+              SizedBox(
+                width: 49,
+                height: 49,
+                child: IconButton(
+                  onPressed: () => appSettingController.isDarkTheme = !isDark,
+                  icon: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 3000),
+                    transitionBuilder: (child, animation) {
+                      return FadeTransition(opacity: animation, child: child);
+                    },
+                    child: isDark
+                      ? const Icon(Icons.light_mode)
+                      : const Icon(Icons.dark_mode),
+                  ),
+                ),
+              ),
+              GameButton(
+                color: playboardDefaultColor,
+                size: ButtonSize.square,
+                child: isMute
+                  ? const Icon(Icons.music_off)
+                  : const Icon(Icons.music_note),
+                onPressed: () => audioController.isMuted = !isMute,
               ),
             ],
           ),
